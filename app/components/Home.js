3bf4386aceb5ab1887640bf3c25bc023ffe17dab
chris <chrisep8@gmail.com>
2017-01-10 12:16:19 +0700

    Add Homepage

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,21 +1,21 @@
 // @flow
-import React, {Component} from 'react';
-import {Link} from 'react-router';
+import React, { Component } from 'react';
+import { Link } from 'react-router';
 import styles from './Home.css';
 
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <div className={styles.container}>
-                    <img src="../resources/images/gitlab-logo.png" alt="" className={styles.logo}/>
-                    <input type="text" className={styles.token} placeholder="Token"/>
-                    <Link to="/counter">
-                        <button className={styles.button}>Authorize</button>
-                    </Link>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className={styles.container}>
+          <img src="../resources/images/gitlab-logo.png" alt="" className={styles.logo} />
+          <input type="text" className={styles.token} placeholder="Token" />
+          <Link to="/counter">
+            <button className={styles.button}>Authorize</button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
